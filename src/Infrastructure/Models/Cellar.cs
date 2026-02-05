b3f60479000054f76db7df1dbacd00b291a34c9f
LauritsGude <laurits.gude@it-minds.dk>
2026-02-05 14:38:39 +0100

    Feature: Created one-to-many relationsships between the different models

--- src/Infrastructure/Models/Cellar.cs
+++ src/Infrastructure/Models/Cellar.cs
@@ -3,4 +3,8 @@
 public class Cellar
 {
     public Guid CellarId { get; set; }
+
+    // Foreign key to user
+    public Guid UserId { get; set; }
+    public List<StorageUnit> StorageUnits { get; set; } = new();
 }
